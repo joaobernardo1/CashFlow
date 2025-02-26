173dc770ea781c2d4f60a2f54acf1689cac3cb71
joaobernardo1 <joaovitorbernardo304@gmail.com>
2025-02-26 18:37:35 -0300

    Adicionando Entities para Banco de dados

---
+++ src/CashFlow.Domain/Entities/Expense.cs
@@ -0,0 +1,13 @@
+﻿using CashFlow.Domain.Enums;
+
+namespace CashFlow.Domain.Entities
+{
+    public class Expense
+    {
+        public string Title = string.Empty;
+        public string? Description;
+        public DateTime Date;
+        public decimal Amount;
+        public PaymentType PaymentType;
+    }
+}
--- src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
+++ src/CashFlow.Domain/obj/Debug/net8.0/CashFlow.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d242ad47a46157e0917c8d5a9d53826d906e5014")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2689ac71f9f6c5c7ba861279b0c3841b2e587fe1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CashFlow.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
